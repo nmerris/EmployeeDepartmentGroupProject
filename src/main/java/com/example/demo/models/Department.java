d9ad7bcade17d7c10a4d6253c94834ac109217cb
nmerris <nate.merris@gmail.com>
2017-08-30 09:42:40 -0400

    initial commit

---
+++ src/main/java/com/example/demo/models/Department.java
@@ -0,0 +1,50 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    // one Director may have many movies
+    // will look for a field called director in another entity
+    // CascadeType.ALL: basically will update everything in both tables at once, also if you delete a director, it
+    // will delete all the movies associated with that director
+    // FetchType.EAGER: will get ALL movies and ALSO ALL entities that are associated with each movie
+    // so maybe a movie would also have 'stage hand' entity data associated with it, which will be available to a Director
+    // as long as EAGER is used for FetchType
+    // LAZY: would ONLY get the movies associated with this director, would NOT get any other entities data 'down the line'
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Set<Employee> employees;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/com/example/demo/models/Employee.java
@@ -0,0 +1,76 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private long cellNumber;
+
+    private long workNumber;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    // director_id is just the name we are choosing to call the JOIN column, you can name it anything you want, but
+    // [entity-name]_id is convention
+    @JoinColumn(name = "department_id")
+//    @NotNull
+    private Department department;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getCellNumber() {
+        return cellNumber;
+    }
+
+    public void setCellNumber(long cellNumber) {
+        this.cellNumber = cellNumber;
+    }
+
+    public long getWorkNumber() {
+        return workNumber;
+    }
+
+    public void setWorkNumber(long workNumber) {
+        this.workNumber = workNumber;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
